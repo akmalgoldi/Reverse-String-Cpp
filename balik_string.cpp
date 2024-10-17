364921bbc6d6b41e4a9707ccb926412b10bdcb74
akmalgoldi <151921630+akmalgoldi@users.noreply.github.com>
2024-10-17 08:00:38 +0700

    Create balik_string.cpp

---
+++ balik_string.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string balikString(string str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+    return str;
+}
+
+int main() {
+    string nama, nim, umur, asal;
+    
+    cout << "Masukkan nama: ";
+    cin >> nama;
+    
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    
+    cout << "Masukkan umur: ";
+    cin >> umur;
+    
+    cout << "Masukkan asal daerah: ";
+    cin >> asal;
+
+    cout << "\nHasil pembalikan string:\n";
+    cout << "Nama dibalik: " << balikString(nama) << endl;
+    cout << "NIM dibalik: " << balikString(nim) << endl;
+    cout << "Umur dibalik: " << balikString(umur) << endl;
+    cout << "Asal daerah dibalik: " << balikString(asal) << endl;
+
+    return 0;
+}
